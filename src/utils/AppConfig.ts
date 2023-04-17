ebde5f232ca2f9755c5241b11e0a4365247b7465
teddzyb <teddybartletg88@gmail.com>
2023-04-17 20:52:43 +0800

    chore: edit app config

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,6 @@
-// FIXME: Update this configuration file based on your project information
-
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Stringer',
+  title: 'Stringer - string manipulator',
+  description: 'Stringer is the best website for string manipulation.',
   locale: 'en',
 };
